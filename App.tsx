2fc855159c408cd237896b67896127d988ed239f
ngocminhk65 <20020061@vnu.edu.vn>
2023-11-12 21:24:16 +0700

    update

--- App.tsx
+++ App.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import  StoryDetail  from './src/screen/StoryDetail';
-// import LoginScreen from './src/screen/Login';
-// import HomeScreen from './src/screen/ChapterDetail';
+import LoginScreen from './src/screen/LoginScreen';
+import SignupScreen from './src/screen/SignupScreen';
 import HomeScreen from './src/screen/HomeScreen';
 import ChapterDetail from './src/screen/ChapterDetail';
 
@@ -13,9 +13,9 @@ function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-          {/* <Stack.Screen name="StoryDetail" component={StoryDetail} /> */}
-        <Stack.Screen name="/HomeScreen" component={HomeScreen} />
-        {/* <Stack.Screen name="/login" component={LoginScreen} /> */} 
+        <Stack.Screen name="LoginScreen" component={LoginScreen} />
+        <Stack.Screen name="SignupScreen" component={SignupScreen} /> 
+        <Stack.Screen name="HomeScreen" component={HomeScreen} />
         <Stack.Screen name="StoryDetail" component={StoryDetail} />
         <Stack.Screen name="ChapterDetail" component={ChapterDetail} />
       </Stack.Navigator>
