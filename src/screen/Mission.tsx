82d130f2e5bbf73e96234370272631d883f285dc
Hoang Trung Kien <81230694+trungkien20020428@users.noreply.github.com>
2023-11-26 22:26:29 +0700

    fix

--- src/screen/Mission.tsx
+++ src/screen/Mission.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, TouchableOpacity, Image, StyleSheet } from 'react-native';
-// import AsyncStorage from '@react-native-async-storage/async-storage';
+import { View, Text, TouchableOpacity, Image, StyleSheet, Alert } from 'react-native';
+ import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const Mission: React.FC = () => {
   const [isClaimed, setIsClaimed] = useState(false);
@@ -11,33 +11,33 @@ const Mission: React.FC = () => {
   }, []);
 
   const handleClaim = async () => {
-    // Kiểm tra xem đã nhận nhiệm vụ chưa và ngày nhận gần nhất
-    // const lastClaimDate = await AsyncStorage.getItem('lastClaimDate');
-    // const currentDate = new Date().toLocaleDateString();
+   // Kiểm tra xem đã nhận nhiệm vụ chưa và ngày nhận gần nhất
+    const lastClaimDate = await AsyncStorage.getItem('lastClaimDate');
+    const currentDate = new Date().toLocaleDateString();
 
-    // if (!isClaimed && lastClaimDate !== currentDate) {
-    //   // Gọi hàm hoặc API để cộng thêm 100 xu cho người dùng (đây chỉ là ví dụ, bạn cần thay thế bằng logic thực tế của bạn)
-    //   // addUserCoins(100);
+    if (!isClaimed && lastClaimDate !== currentDate) {
+      // Gọi hàm hoặc API để cộng thêm 100 xu cho người dùng (đây chỉ là ví dụ, bạn cần thay thế bằng logic thực tế của bạn)
+      // addUserCoins(100);
 
-    //   // Lưu lại ngày nhận xu gần nhất
-    //   await AsyncStorage.setItem('lastClaimDate', currentDate);
+      // Lưu lại ngày nhận xu gần nhất
+      await AsyncStorage.setItem('lastClaimDate', currentDate);
 
-    //   // Đánh dấu nhiệm vụ đã được nhận
-    //   setIsClaimed(true);
-    //   alert('Bạn đã nhận nhiệm vụ thành công! Bạn đã được thưởng 100 xu.');
-    // } else {
-    //   alert('Bạn đã nhận nhiệm vụ trong ngày hôm nay hoặc đã nhận trước đó.');
-    // }
+      // Đánh dấu nhiệm vụ đã được nhận
+      setIsClaimed(true);
+      Alert.alert('Bạn đã nhận nhiệm vụ thành công! Bạn đã được thưởng 100 xu.');
+    } else {
+      Alert.alert('Bạn đã nhận nhiệm vụ trong ngày hôm nay hoặc đã nhận trước đó.');
+    }
   };
 
   const checkClaimStatus = async () => {
     // Kiểm tra trạng thái khi component được render
-    // const lastClaimDate = await AsyncStorage.getItem('lastClaimDate');
+     const lastClaimDate = await AsyncStorage.getItem('lastClaimDate');
     const currentDate = new Date().toLocaleDateString();
 
-    // if (lastClaimDate === currentDate) {
-    //   setIsClaimed(true);
-    // }
+    if (lastClaimDate === currentDate) {
+      setIsClaimed(true);
+    }
   };
 
   return (
